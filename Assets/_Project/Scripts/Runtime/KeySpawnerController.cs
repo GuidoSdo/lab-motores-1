fe68d37d6e8cbf6b416f7114fd9e96b121e7fe97
Denis Liam Cruz 42956023 <denisliamcruz@hotmail.com>
2026-06-05 22:22:12 -0300

    Feat: Key functionality Level2

--- Assets/_Project/Scripts/Runtime/KeySpawnerController.cs
+++ Assets/_Project/Scripts/Runtime/KeySpawnerController.cs
@@ -2,16 +2,16 @@ using UnityEngine;
 
 public class KeySpawnerController : MonoBehaviour
 {
-
-
     [SerializeField] private GameObject key;
     [SerializeField] private Transform pos1;
     [SerializeField] private Transform pos2;
 
-
     void Start()
     {
-        
-    }
+        Transform randomPos =
+            Random.Range(0, 2) == 0 ? pos1 : pos2;
 
+        key.transform.position = randomPos.position;
+        key.transform.rotation = randomPos.rotation;
+    }
 }
